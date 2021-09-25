79dc84680dfff17bb2fba490e4a47ef399a5c122
Mariane Thomson <marianedinis@gmail.com>
2021-09-25 12:32:47 +1000

    new work

--- Assets/js/scripts.js
+++ Assets/js/scripts.js
@@ -1,13 +1,8 @@
 //store blocks of hours - it can be reduced or expanded - currently displaying business hours (9am-5pm)
 var dailyHours = 9;
 
-//store text ref hour - first time block on the planner
-var hour = 9;
-
-//creates an array to store all the daily events
-var plannedActivities =[];
-
-var saveBtns = $(".col-1 saveBtn");
+//store saved activities
+var savedActivities;
 
 //show current day - header
 function displayToday() {
@@ -19,7 +14,7 @@ function setBGColour(el, textTime, currTime) {
   if (textTime > currTime) {
     el.addClass("future");
   } else if (textTime < currTime) {
-    el.addClass("past");
+    el.addClass("past").css("pointer-events", "none");
   } else {
     el.addClass("present");
   }
@@ -27,46 +22,57 @@ function setBGColour(el, textTime, currTime) {
 
 //generate and populate the html elements for the time blocks passing how many hours the planner should display (see var dailyHours)
 function generateTimeBlocks(hours) {
+  //store text ref hour - first time block on the planner considering dailyHours as 9am-5pm
+  var hour = 9;
+
+  //get the current hours using Moments js
   var currentHour = moment().get("hour");
+
+  //loop to create the time-blocks and their elements
   for (var i = 0; i < hours; i++) {
+    //format AM/PM using Moments js
     var formatedTime = moment(hour, "H").format("h a");
-    $timeBlockDiv = $("<div>").addClass("row time-block");
 
-    $timeText = $("<h4>").text(formatedTime);
-    $timeDiv = $("<div>").addClass("col-2 hour").append($timeText);
+    //creates the div for each interation and styles the class
+    var $timeBlockDiv = $("<div>").addClass("row time-block");
 
-    $descriptionDiv = $("<textarea>").addClass("col-9 description").text("");
+    //creates the element to hold text ref hour, passes the hour number for each interation formatted as h am/pm
+    var $time = $("<h4>").text(formatedTime).addClass("col-2 hour");
 
-    setBGColour($descriptionDiv, hour, currentHour);
+    //creates the element for the activity and styles the textarea and sets its id as each hour/interation
+    var $activityTxtArea = $("<textarea>").addClass("col-9 description");
 
-    $saveBtn = $("<button>")
+    //creates the activity obj
+    var $activity = {
+      index: hour,
+      text: $activityTxtArea.val(),
+    };
+
+    $activityTxtArea.append($activity);
+
+    //screates the save btn, styles the save button and sets its id as each hour/interation
+    var $saveBtn = $("<button>")
       .text("Save")
       .addClass("col-1 saveBtn")
-      .attr("id-hour", hour);
-    $saveDiv = $("<div>").addClass("saveBtn").append($saveBtn);
+      .attr("id", hour);
 
-    $timeBlockDiv.append($timeDiv, $descriptionDiv, $saveBtn);
+    //display to the page
+    $timeBlockDiv.append($time, $activityTxtArea, $saveBtn);
+
+    setBGColour($activityTxtArea, hour, currentHour);
+    setBGColour($saveBtn, hour, currentHour);
 
     $(".container").append($timeBlockDiv);
+
     hour++;
+
+    $saveBtn.on("click", function (event) {
+      event.preventDefault();
+      // savedActivities.splice($activity.index, 0, $activity.text);
+      localStorage.setItem("activity", JSON.stringify($activity));
+    });
   }
 }
-
-//capture the scheduled plan and save it on click
-
-
-saveBtns.on("click", "input", function (event) {
-  event.preventDefault();
-
-  var activity ={
-      hour = $("#id-hour"),
-      description = input.value.trim(),
-  }
-
-  plannedActivities[activity.hour] = activity;
-
-  localStorage.setItem("plannedActivities", JSON.stringify(plannedActivities));
-});
 
 window.onload = (event) => {
   displayToday();
